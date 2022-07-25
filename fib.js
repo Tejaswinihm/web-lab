8fa02ce30b200b6c4345334a476493e5fdd4ce82
tejaswini h m <100576246+Tejaswinihm@users.noreply.github.com>
2022-07-25 16:05:06 +0530

    Add files via upload

---
+++ fib.js
@@ -0,0 +1,11 @@
+//fibonacci numbers
+var n;
+n=parseInt(prompt("enter a numbers"))
+msg="the first"+n + " numbers are \n "
+for(i=1;i<=n;i++){
+    //document.write(i + "<br />")
+    msg+=i + "\n";
+
+}
+
+alert(msg);
